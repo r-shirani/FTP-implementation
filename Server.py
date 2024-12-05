62fca7078dd1aa0fca57110d21636faffb2f5508
R_shirani <127476888+r-shirani@users.noreply.github.com>
2024-12-05 15:20:21 +0330

    server file was created

---
+++ Server.py
@@ -0,0 +1,7 @@
+import socket
+import os
+import time
+
+host = '127.0.0.1' #local host
+control_port = 21  #standard control port
+data_port = 2021   #can be 2021, 2121, 3000 etc for transfering data
