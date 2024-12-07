6c1f5240cab4615dfad1f44fee35754f1862b61f
R_shirani <127476888+r-shirani@users.noreply.github.com>
2024-12-07 14:20:48 +0330

    Add FTPS with SSL to Client.py

--- Client.py
+++ Client.py
@@ -1,21 +1,24 @@
-
 import socket
+import ssl
 
 def Client():
     host = '127.0.0.1' #local host
     control_port = 21  #standard control port
-    data_port = 2021   #can be 2021, 2121, 3000 etc for transfering data
+    data_port = 2021   #can be 2021, 2121, 3000, etc for transferring data
 
     #TCP socket for connecting to the server(IPv4,TCP)
-    control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    #connect the client to the server for transferiing the commands
+    control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #secure control socket
+    control_socket = ssl.wrap_socket( control_socket, ssl_version= ssl.PROTOCOL_TLS )
+    #connect the client to the server to transfer the commands
     control_socket.connect((host, control_port))
 
     #do the same things for transferring data
     data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    data_socket = ssl.wrap_socket( data_socket, ssl_version= ssl.PROTOCOL_TLS)
     data_socket.connect((host, data_port))
 
-    #send commands to the server and get result
+    #send commands to the server and get the result
     def send_command(command):
         control_socket.send(f"{command}\r\n".encode())# send by converting the string to the bytes
         result = control_socket.recv(1024).decode()#get the result and convert max 1024 byte to string
@@ -35,7 +38,7 @@ def Client():
            if result.startswith('    *150*'):    #preparing to transfer the file 
              filename = command.split()[1]       #file name
              with open(filename, "wb") as file:  #open file in writting mode
-                  data = data_socket.recv(1024)  #receive the file via data soket
+                  data = data_socket.recv(1024*1024*10)  #receive the file via data socket
                   file.write(data)  #write the data in the file
              print(control_socket.recv(1024).decode()) #print the result from the server
         elif command.upper().startswith("STOR"): #for uploading a file
